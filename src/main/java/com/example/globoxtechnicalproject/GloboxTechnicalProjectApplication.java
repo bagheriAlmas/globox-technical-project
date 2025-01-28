818adb73f316c6ae9a279de9c67f86c5c80bed08
Mehdi Bagheri <m.bagheri@negargaran.local>
2025-01-28 13:28:17 +0330

    first commit

---
+++ src/main/java/com/example/globoxtechnicalproject/GloboxTechnicalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.globoxtechnicalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GloboxTechnicalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GloboxTechnicalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/globoxtechnicalproject/GloboxTechnicalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.globoxtechnicalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GloboxTechnicalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
